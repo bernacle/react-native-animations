f3f74778d272717c6cc93cece8744ee15244b6f1
F1694326 Bruno Raphael Bifaroni Simplicio <brunoraphael@bb.com.br>
2020-07-18 15:59:40 -0300

    loopping cards

--- screens/ProjectsScreen.jsx
+++ screens/ProjectsScreen.jsx
@@ -1,21 +1,36 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useCallback, useRef } from "react";
 import { View, PanResponder, Animated } from "react-native";
 import styled from "styled-components";
 import Project from "../components/Project";
 import { projects } from "../utils/projects";
 
+function getNextIndex(ind) {
+  let nextIndex = ind + 1;
+  if (nextIndex > projects.length - 1) {
+    return 0;
+  }
+  return nextIndex;
+}
+
 const ProjectsScreen = () => {
   const pan = useRef(new Animated.ValueXY()).current;
   const scale = useRef(new Animated.Value(0.9)).current;
   const translateY = useRef(new Animated.Value(44)).current;
+  const thirdScale = useRef(new Animated.Value(0.8)).current;
+  const thirdTranslateY = useRef(new Animated.Value(-50)).current;
+  const [index, setIndex] = useState(0);
+
   const panResponder = useRef(
     PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
       onPanResponderGrant: () => {
         // Here we animate the second card to the first card initial state
         Animated.spring(scale, { toValue: 1 }).start();
         Animated.spring(translateY, { toValue: 0 }).start();
+        Animated.spring(thirdScale, { toValue: 0.9 }).start();
+        Animated.spring(thirdTranslateY, { toValue: 44 }).start();
       },
-      onMoveShouldSetPanResponder: () => true,
+
       onPanResponderMove: Animated.event([
         null,
         {
@@ -25,15 +40,23 @@ const ProjectsScreen = () => {
       ]),
       onPanResponderRelease: () => {
         const positionY = pan.y._value;
-
         if (positionY > 200) {
           Animated.timing(pan, {
             toValue: { x: 0, y: 1000 },
-          }).start();
+          }).start(() => {
+            pan.setValue(new Animated.ValueXY());
+            scale.setValue(0.9);
+            translateY.setValue(44);
+            thirdScale.setValue(0.8);
+            thirdTranslateY.setValue(-50);
+            setIndex(index => getNextIndex(index));
+          });
         } else {
           Animated.spring(pan, { toValue: { x: 0, y: 0 } }).start();
           Animated.spring(scale, { toValue: 0.9 }).start();
           Animated.spring(translateY, { toValue: 44 }).start();
+          Animated.spring(thirdScale, { toValue: 0.8 }).start();
+          Animated.spring(thirdTranslateY, { toValue: -50 }).start();
         }
       },
     }),
@@ -46,10 +69,10 @@ const ProjectsScreen = () => {
         {...panResponder.panHandlers}
       >
         <Project
-          title={projects[1].title}
-          image={projects[1].image}
-          author={projects[1].author}
-          text={projects[1].text}
+          title={projects[index].title}
+          image={projects[index].image}
+          author={projects[index].author}
+          text={projects[index].text}
         />
       </Animated.View>
       <Animated.View
@@ -66,10 +89,30 @@ const ProjectsScreen = () => {
         }}
       >
         <Project
-          title={projects[0].title}
-          image={projects[0].image}
-          author={projects[0].author}
-          text={projects[0].text}
+          title={projects[getNextIndex(index)].title}
+          image={projects[getNextIndex(index)].image}
+          author={projects[getNextIndex(index)].author}
+          text={projects[getNextIndex(index)].text}
+        />
+      </Animated.View>
+      <Animated.View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: -2,
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          transform: [{ scale: thirdScale }, { translateY: thirdTranslateY }],
+        }}
+      >
+        <Project
+          title={projects[getNextIndex(index + 1)].title}
+          image={projects[getNextIndex(index + 1)].image}
+          author={projects[getNextIndex(index + 1)].author}
+          text={projects[getNextIndex(index + 1)].text}
         />
       </Animated.View>
     </Container>
